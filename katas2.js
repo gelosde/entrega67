946261478572627137a2112352fd99fd38bfefd3
gelosde <85689579+gelosde@users.noreply.github.com>
2021-06-17 20:13:10 -0300

    tudo corrigido, completo e testado

--- katas2.js
+++ katas2.js
@@ -1,79 +1,59 @@
-// comece a criar a sua função add na linha abaixo
 function add(soma1,soma2){
-return soma1+soma2;
-}
-
-// descomente a linha seguinte para testar sua função
+    return soma1+soma2;
+    }
  console.assert(add(3, 5) === 8,'A função add não está funcionando como esperado');
+function multiply(multiplicado,multiplicador){
+    let total=0;
+    for(let cont=0;cont<multiplicador;cont++){
+    if(cont===1){
+    total=(add(multiplicado,multiplicado));
+        
+}else{
+        total =add(total,multiplicado);
+    }
+}   
+    
+    
+   return total;
+  }
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
+ function power(valor,potencia){
+    let total=0;   
+    for(cont=1;cont<potencia;cont++){
+    if(cont===1){
+        total= multiply(valor,valor);
+    }else{
+        total=multiply(total,valor);
+    }return total;}
+    }
 
-// comece a criar a sua função multiply na linha abaixo
-        function multiply(multiplicado,multiplicador){
-            let total=0;
-            for(let cont=1;cont<multiplicador;cont++){
-            if(cont===1){
-            total=(add(multiplicado,multiplicado));
-                
-        }else{
-                total =add(total,multiplicado);
-            }
-        }   
-            
-            
-           return total;
-          }
-          function power(valor,potencia){
-            let total=0;   
-            for(cont=1;cont<potencia;cont++){
-            if(cont===1){
-                total= multiply(valor,valor);
-            }else{
-                total=multiply(total,valor);
-            }
-            }
-          
-            return total;
-        }
-
-// descomente a linha seguinte para testar sua função
+  
  console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
-
-// comece a criar a sua função factorial na linha abaixo
-
-function factorial(fato){
-    let cont;
+function fatorial(fato){
     let valor=1;
-    for(cont=1;cont<=fato;cont++){
-        valor =valor*cont;
+    for(let cont =1;cont<=fato;cont++){
+        valor=multiply(valor,cont)
     }
-    return valor
-}
-// descomente a linha seguinte para testar sua função
+    return valor;
+  }
+    
  console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-function fibonacci(fbo){
-    let cont ;
-    let arr=[];
-    for(cont=0;cont<fbo;cont++){
-        if(cont===0){
-            arr[cont]=cont;
-        }else if(cont===1){
-            arr[cont]=cont;
-        }
-        else{
-            
-            arr[cont]=add(arr[cont-2],arr[cont-1]);
-        }
-    }return arr[fbo];
-
+function fibonacci(fbo) {
+let cont;
+let arr = [];
+for (cont = 0; cont < fbo; cont++) {
+if (cont === 0) {
+arr[cont] = cont;
+} else if (cont === 1) {
+arr[cont] = cont;
+} else {
+arr[cont] = add(arr[cont - 2], arr[cont - 1]);
 }
-// descomente a linha seguinte para testar sua função
+}
+return arr[fbo-1];
+}
+
 console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
